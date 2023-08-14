d1e25902d02db14649cc4dc594b8c25841d19cc2
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-08-15 00:46:07 +0300

    Category list operation added

--- 50DersteMVC/Controllers/CategoriesController.cs
+++ 50DersteMVC/Controllers/CategoriesController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _50DersteMVC.Models.Entity;
 
 namespace _50DersteMVC.Controllers
 {
     public class CategoriesController : Controller
     {
         // GET: Default
+        Entities entities = new Entities();
         public ActionResult Index()
         {
-            return View();
+            var categories = entities.Categories.ToList();
+            return View(categories);
         }
     }
 }
